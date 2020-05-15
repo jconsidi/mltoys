00ee2c89dbd162ee0a5f5fe5d3866adff256af80
Jeffrey Considine <jconsidi@gmail.com>
2020-05-14 21:34:58 -0400

    + blacken

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 
-setup(name = 'mltoys',
-      version = '0.1.0',
-      packages = ['mltoys', 'mltoys.factories'],
-      )
+setup(
+    name="mltoys", version="0.1.0", packages=["mltoys", "mltoys.factories"],
+)
